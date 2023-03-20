b8a831bbed74733112495768f55044e2f1fb5068
DatPipBoy <116214829+DatPipBoy@users.noreply.github.com>
2023-03-20 18:28:17 -0400

    Created script and added computer selection logic

---
+++ script.js
@@ -0,0 +1,21 @@
+//Logic for computer choice.
+
+
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 3); {
+        if (computerChoice === 0) {
+            return "Rock"
+        } else if 
+        (computerChoice === 1) {
+            return "Paper"
+        } else if 
+        (computerChoice === 2) {
+            return "Scissors"
+        }
+    }
+}
+
+getComputerChoice();
+
+console.log(getComputerChoice())
+
