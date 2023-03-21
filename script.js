0d26d3494abe2c905a4d5a17801ea0d24f119962
DatPipBoy <116214829+DatPipBoy@users.noreply.github.com>
2023-03-21 08:20:15 -0400

    Added Logic for player choice input

--- script.js
+++ script.js
@@ -4,18 +4,39 @@
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3); {
         if (computerChoice === 0) {
-            return "Rock"
+            return "rock"
         } else if 
         (computerChoice === 1) {
-            return "Paper"
+            return "paper"
         } else if 
         (computerChoice === 2) {
-            return "Scissors"
+            return "scissors"
         }
     }
 }
 
 getComputerChoice();
 
-console.log(getComputerChoice())
+//Logic for Player choice.
+
+const rock = document.getElementById("rock");
+
+rock.addEventListener("click", function(e) {
+    return "rock"
+});
+
+const paper = document.getElementById("paper");
+
+paper.addEventListener("click", function(e) {
+    return "paper"
+});
+
+const scissors = document.getElementById("scissors");
+
+scissors.addEventListener("click", function(e) {
+    return "scissors"
+});
+
+//Logic for result output
+
 
